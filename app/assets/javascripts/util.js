bb640bb7cc5d3e93de996d8610954ec09552f465
DanielsLuz <danielluz1995@gmail.com>
2016-08-26 09:57:11 -0300

    Fixed diff arrays function logic.

--- app/assets/javascripts/util.js
+++ app/assets/javascripts/util.js
@@ -1,9 +1,20 @@
 var diffObjects = function(newArray, oldArray){
   var difference = [];
-  $(newArray).each(function(index, el){
-    if(!oldArray.some(function(e){e.id == el.id})){
-      difference.push(el)
+  $(newArray).each(function(index, new_elem){
+    // if(oldArray.filter(function(e){e.id == el.id}).length == 0){
+    if(!containsObject(new_elem, oldArray)){
+      difference.push(new_elem)
     }
   })
   return difference;
+}
+
+function containsObject(obj, oldArray) {
+  contains = 0;
+  $(oldArray).each(function(index, elem){
+    if (elem.id == obj.id) {
+      contains = 1;
+    }
+  })
+  return contains;
 }
